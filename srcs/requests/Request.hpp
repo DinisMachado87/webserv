8f40e1fc10a84fa134c7d78e7117c6d91afb8c6f
smoon <smoon@student.42berlin.com>
2026-03-04 14:29:49 +0100

    added Request header

--- srcs/requests/Request.hpp
+++ srcs/requests/Request.hpp
@@ -13,15 +13,14 @@ enum requestMethod : unsigned char
 };
 
 							//RFC2616
-typedef	struct	reqVars	{	//{defaults}
-requestMethod	method;		//GET / POST / PUT / DELETE etc
-int		contentLength;		//length of message body - taken from header or manually calculated if chunked {-1}
-char*	CONTENT_TYPE;		//media type of message body - from header {NULL}
-char*	PATH_INFO;			//path after the file name as presented in URL {NULL} e.g. URL=example.com/cgi-bin/hello.cgi/user/admin PATH_INFO=/user/admin
-char*	QUERY_STRING;		//information for the CGI script to affect the return value - URL after '?' {NULL} e.g. URL=example.com/cgi-bin/hello.cgi/user/admin?query=date QUERY_STRING=query=date
-char*	REMOTE_ADDR;		//network address of client sending the request (ipv4 or ipv6) {NULL}
-char*	REMOTE_HOST;		//domain name of the client sending the request, or {NULL}
-char*	SCRIPT_NAME;		//path and name of the .cgi file to be run {NULL} e.g. URL=example.com/cgi-bin/hello.cgi/user/admin SCRIPT_NAME=/cgi-bin/hello.cgi
+typedef	struct	reqVars	{		//{defaults}
+requestMethod	method;			//GET / POST / PUT / DELETE etc
+int				contentLength;	//length of message body - taken from header or manually calculated if chunked {-1}
+std::string		requestPath;	//e.g. URL=example.com/cgi-bin/hello.cgi/user/admin {NULL}
+char*			CONTENT_TYPE;	//media type of message body - from header {NULL}
+char*			QUERY_STRING;	//information for the CGI script to affect the return value - URL after '?' {NULL} e.g. URL=example.com/cgi-bin/hello.cgi/user/admin?query=date QUERY_STRING=query=date
+char*			REMOTE_ADDR;	//network address of client sending the request (ipv4 or ipv6) {NULL}
+char*			REMOTE_HOST;	//domain name of the client sending the request, or {NULL}
 }	reqVars;
 
 
