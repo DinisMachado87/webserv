5046b965e181e6c3cfa83f6419a9bceb59b09259
Aviv Kosloff <aviv.film@gmail.com>
2026-03-10 19:27:52 +0100

    updated request class with constructor that can be used by parser and with getters

--- srcs/requests/Request.hpp
+++ srcs/requests/Request.hpp
@@ -4,6 +4,7 @@
 
 
 struct Location;
+class HttpRequest;
 
 enum requestMethod : unsigned char
 {
@@ -17,11 +18,10 @@ typedef	struct	reqVariables {		//{defaults}
 requestMethod	method;			//GET / POST / PUT / DELETE etc
 int				contentLength;	//length of message body - taken from header or manually calculated if chunked {-1}
 std::string		requestPath;	//e.g. URL=example.com/cgi-bin/hello.cgi/user/admin {NULL} (parser needs to check for cgi-bin)
-//these below might change to std::string
-char*			CONTENT_TYPE;	//media type of message body - from header {NULL}
-char*			QUERY_STRING;	//information for the CGI script to affect the return value - URL after '?' {NULL} e.g. URL=example.com/cgi-bin/hello.cgi/user/admin?query=date QUERY_STRING=query=date
-char*			REMOTE_ADDR;	//network address of client sending the request (ipv4 or ipv6) {NULL}
-char*			REMOTE_HOST;	//domain name of the client sending the request, or {NULL}
+std::string		CONTENT_TYPE;	//media type of message body - from header {NULL}
+std::string		QUERY_STRING;	//information for the CGI script to affect the return value - URL after '?' {NULL} e.g. URL=example.com/cgi-bin/hello.cgi/user/admin?query=date QUERY_STRING=query=date
+std::string		REMOTE_ADDR;	//network address of client sending the request (ipv4 or ipv6) {NULL}
+std::string		REMOTE_HOST;	//domain name of the client sending the request, or {NULL}
 }	reqVariables;
 
 
@@ -29,8 +29,14 @@ class	Request
 {
 public:
 	Request(Location* loc);
+	Request(Location* loc, const HttpRequest& parsed, int clientFD,
+		const std::string& remoteAddr, const std::string& remoteHost);
 	~Request(void);
 
+	const reqVariables&	getVariables() const;
+	const std::string&	getBody() const;
+	int					getClientFD() const;
+	Location*			getLocation() const;
 
 protected:
 	std::string	_body;
